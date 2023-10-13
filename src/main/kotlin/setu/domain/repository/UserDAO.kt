e9dac016ccca0c464f8efdf1ff139d312745db12
TokarevIP <tokarevrab@gmail.com>
2023-10-13 18:48:57 +0100

    Change Setu Folder Path

--- src/main/kotlin/setu/domain/repository/UserDAO.kt
+++ src/main/kotlin/setu/domain/repository/UserDAO.kt
@@ -1,5 +1,6 @@
 package setu.domain.repository
 
+import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
 import setu.domain.User
@@ -8,40 +9,46 @@ import setu.utils.mapToUser
 
 class UserDAO {
 
-//    private val users = arrayListOf(
-//        User(name = "Alice", email = "alice@wonderland.com", id = 0),
-//        User(name = "Bob", email = "bob@cat.ie", id = 1),
-//        User(name = "Mary", email = "mary@contrary.com", id = 2),
-//        User(name = "Carol", email = "carol@singer.com", id = 3)
-//    )
-
     fun getAll(): ArrayList<User> {
         val userList: ArrayList<User> = arrayListOf()
         transaction {
             Users.selectAll().map {
-                userList.add(mapToUser(it)) }
+                userList.add(mapToUser(it))
+            }
         }
         return userList
     }
 
     fun findById(id: Int): User? {
-        return null
+        return transaction {
+            Users.select() {
+                Users.id eq id
+            }
+                .map { mapToUser(it) }
+                .firstOrNull()
+        }
     }
 
-    fun findByEmail(email: String) :User?{
-        return null
+    fun findByEmail(email: String): User? {
+        return transaction {
+            Users.select() {
+                Users.email eq email
+            }
+                .map { mapToUser(it) }
+                .firstOrNull()
+        }
     }
 
-    fun save(user: User){
+    fun save(user: User) {
 //        users.add(user)
     }
 
-    fun delete(id: Int){
+    fun delete(id: Int) {
 //        val userById = users.find { it.id == id }
 //        users.remove(userById)
     }
 
-    fun update(id: Int, user: User){
+    fun update(id: Int, user: User) {
 //        val foundUser = findById(id)
 //        foundUser?.email = user.email
 //        foundUser?.name = user.name
