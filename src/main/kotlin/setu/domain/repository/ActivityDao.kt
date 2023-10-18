d022d9407bf637288b5c90772842ac663291c5dd
TokarevIP <tokarevrab@gmail.com>
2023-10-18 15:01:03 +0100

    Resolves #1

--- src/main/kotlin/setu/domain/repository/ActivityDao.kt
+++ src/main/kotlin/setu/domain/repository/ActivityDao.kt
@@ -1,8 +1,6 @@
 package setu.domain.repository
 
-import org.jetbrains.exposed.sql.insert
-import org.jetbrains.exposed.sql.select
-import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.transaction
 import setu.domain.Activity
 import setu.domain.db.Activities
@@ -20,19 +18,62 @@ class ActivityDao {
         return activityList
     }
 
-    fun findByUserId(userDd: Int): Activity? {
-        return transaction {
+    fun findByUserId(userDd: Int): ArrayList<Activity> {
+        val activityList: ArrayList<Activity> = arrayListOf()
+        transaction {
             Activities.select() {
                 Activities.userId eq userDd
+            }
+                .map {
+                    activityList.add(mapToActivity(it))
+                }
+        }
+        return activityList
+    }
+
+    fun save(activity: Activity) {
+        transaction {
+            Activities.insert {
+                it[description] = activity.description
+                it[duration] = activity.duration
+                it[calories] = activity.calories
+                it[started] = activity.started
+                it[userId] = activity.userId
+            }
+        }
+    }
+
+    fun deleteByUserId(userId: Int) {
+        transaction {
+            Activities.deleteWhere {
+                Activities.userId eq userId
+            }
+        }
+    }
+
+    fun findById(id: Int): Activity? {
+        return transaction {
+            Activities.select() {
+                Activities.id eq id
             }
                 .map { mapToActivity(it) }
                 .firstOrNull()
         }
     }
 
-    fun save(activity: Activity) {
+    fun deleteById(id: Int) {
         transaction {
-            Activities.insert {
+            Activities.deleteWhere {
+                Activities.id eq id
+            }
+        }
+    }
+
+    fun update(id: Int, activity: Activity) {
+        transaction {
+            Activities.update({
+                Activities.id eq id
+            }) {
                 it[description] = activity.description
                 it[duration] = activity.duration
                 it[calories] = activity.calories
